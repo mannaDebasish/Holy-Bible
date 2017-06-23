e0dcbc4663c13f9313912fcddfeb75b21f01eaaa
souravdutta <dutta.sourav840@gmail.com>
2017-06-23 11:38:06 +0530

    --2nd

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express();
 
+var bodyParser = require('body-parser');
+var path = require('path');
+var crypto = require('crypto');
+
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://sourav_dutta:delgence55@ds159371.mlab.com:59371/project_management');
@@ -14,10 +18,93 @@ db.once('open', function() {
 });
 
 app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+
+var personSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    role : {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String
+});
+
+personSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+personSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
+
+var Person = mongoose.model("Users", personSchema);
+
+
+app.post('/register', function(req,res){
+
+    console.log(req.body);
+    var newPerson = new Person({
+        name: req.body.name,
+        address: req.body.address,
+        email: req.body.email,
+        role : req.body.role
+    });
+    var cryptPass = newPerson.setPassword(req.body.password);
+    console.log(cryptPass);
+    newPerson.save(function(err, Person){
+        if(err)
+            res.status(500).send();
+        else
+            res.send(Person);
+        console.log('Registered!');
+    });
+
+});
+
+
+app.post('/login', function(req,res){
+    console.log(req.body);
+    Person.findOne({email: req.body.email}, function(err, Person){
+        if(err)
+            res.status(500).send();
+        else
+        if(Person == null){
+            console.log('not found');
+            res.status(404).send();
+        }
+        else{
+
+            if (!Person.validPassword(req.body.password)) {
+                console.log('wrong pass');
+            }
+           else{
+                console.log(Person);
+                res.send(Person);
+                console.log('Logged In!');
+            }
+        }
+    })
+});
+
 
 
 // error handling
