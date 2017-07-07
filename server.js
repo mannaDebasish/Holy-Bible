a7f2654495e916e5c1efbafce782f657bb632c25
sourav-sd <dutta.sourav840@gmail.com>
2017-07-07 18:18:33 +0530

    Bible Change

--- server.js
+++ server.js
@@ -1,110 +1,175 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express();
-
-var bodyParser = require('body-parser');
-var path = require('path');
-var crypto = require('crypto');
-
-
+var express = require('express');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://sourav_dutta:delgence55@ds159371.mlab.com:59371/project_management');
+var fs = require('fs');
+var crypto = require('crypto');
+var jsonfile = require('jsonfile');
+var app = express();
+mongoose.Promise = global.Promise;
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+/*------------------------ Connect Mongoose ----------------*/
+mongoose.connect('mongodb://debasish:bible@ds151232.mlab.com:51232/bible');
 
 var db = mongoose.connection;
 
 db.once('open', function() {
-    console.log('MongoDB Successfully Connected!!');
+  console.log('MongoDB Successfully Connected!!');
 });
 
-app.use(express.static(__dirname + '/views'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var filepath = 'bible/books/';
 
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-
+/*================== User schema ==============*/
 
 var personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    address: String,
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    role : {
-        type: String,
-        required: true
-    },
-    hash: String,
-    salt: String
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  contact_number : {
+    type: String,
+    required: true
+  },
+  bible_name:String,
+  chapter_name:String,
+  hash: String,
+  salt: String
 });
 
 personSchema.methods.setPassword = function(password){
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
 personSchema.methods.validPassword = function(password) {
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-    return this.hash === hash;
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
 };
 
 var Person = mongoose.model("Users", personSchema);
 
 
+
+/*----------------------------------- Webservices -------------------------------*/
+
 app.post('/register', function(req,res){
 
-    console.log(req.body);
-    var newPerson = new Person({
-        name: req.body.name,
-        address: req.body.address,
-        email: req.body.email,
-        role : req.body.role
-    });
-    var cryptPass = newPerson.setPassword(req.body.password);
-    console.log(cryptPass);
-    newPerson.save(function(err, Person){
-        if(err)
-            res.status(500).send();
-        else
-            res.send(Person);
-        console.log('Registered!');
-    });
+  console.log(req.body);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.set("Content-Type",'application/text');
+
+  var newPerson = new Person({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    contact_number : req.body.contact_number,
+    bible_name : '',
+    chapter_name : ''
+  });
+  var cryptPass = newPerson.setPassword(req.body.password);
+  newPerson.save(function(err, Person){
+    if(err)
+      res.status(500).send();
+    else
+      res.send(Person);
+      res.status(200).send();
+    console.log('User Successfully Registered!');
+  });
 
 });
-
 
 app.post('/login', function(req,res){
-    console.log(req.body);
-    Person.findOne({email: req.body.email}, function(err, Person){
-        if(err)
-            res.status(500).send();
-        else
-        if(Person == null){
-            console.log('not found');
-            res.status(404).send();
-        }
-        else{
+  console.log(req.body);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.set("Content-Type",'application/text');
+  Person.findOne({email: req.body.email}, function(err, Person){
+    if(err)
+      res.status(500).send();
+    else
+    if(Person == null){
+      console.log('not found');
+      res.status(404).send();
+    }
+    else{
 
-            if (!Person.validPassword(req.body.password)) {
-                console.log('wrong pass');
-            }
-           else{
-                console.log(Person);
-                res.send(Person);
-                console.log('Logged In!');
-            }
-        }
-    })
+      if (!Person.validPassword(req.body.password)) {
+        console.log('wrong pass');
+      }
+      else{
+        console.log(Person);
+        res.send(Person);
+        console.log('Logged In!');
+      }
+    }
+  })
 });
 
+app.post('/updatebook', function(req,res){
+  console.log(req.body);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.set("Content-Type",'application/text');
+
+  Person.update({'_id':req.body._id}, { bible_name: req.body.bible_name }, function(err, Person){
+    if(err)
+      res.status(500).send();
+    if(Person){
+      console.log(Person);
+      res.status(200).send('updated book name');
+    }
+
+  })
+});
+
+
+
+
+app.get('/getbiblelist', function(req, res){
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.set("Content-Type",'application/json');
+
+  jsonfile.readFile('bible/booklist/bookname.json', function(err, obj) {
+    //console.log(obj);
+    res.send(obj);
+  });
+});
+
+
+app.get('/bible/:bookName', function(req, res){
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.set("Content-Type",'application/json');
+
+  var bookname = req.params.bookName;
+  console.log(bookname);
+  var bookpath = filepath + bookname +'.json';
+    jsonfile.readFile(bookpath, function(err, obj) {
+    //console.log(obj);
+    res.send(obj);
+  });
+
+});
 
 
 // error handling
